d1b366161dc36624ed1cfff1d6fc9b8ebf731331
Amina Shellman <aminashellman@gmail.com>
2025-03-15 15:21:26 -0700

    add link to album viewer in main file

--- photoclub-web/pages/index.tsx
+++ photoclub-web/pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
           priority
         /> 
          <button className="bg-orange-600 hover:bg-orange-400 text-white hover:text-white font-bold py-2 px-4 rounded" onClick={Download}>
-          <a href="#">Download Photos</a> 
+          <Link href="/photos">Download Photos</Link> 
         </button>
         <button className="bg-orange-600 hover:bg-orange-400 text-white hover:text-white font-bold py-2 px-4 rounded">
           <Link href="/login">Login</Link>
