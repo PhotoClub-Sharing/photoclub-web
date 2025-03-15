16692ff37987b7310f9ab5dd550a73bb126e03fc
Amina Shellman <aminashellman@gmail.com>
2025-03-15 14:51:35 -0700

    fix google login button text color

--- photoclub-web/pages/login.tsx
+++ photoclub-web/pages/login.tsx
@@ -13,7 +13,7 @@ export default function Login() {
     <Layout>
     <div className="flex flex-col items-center justify-center h-screen bg-[#FFBC51]">
       <h1 className="">Login</h1>
-      <button className="px-4 py-2 mt-4 bg-white rounded-md" onClick={GoogleLogin}>Continue with Google</button>
+      <button className="px-4 py-2 mt-4 bg-white text-black rounded-md" onClick={GoogleLogin}>Continue with Google</button>
       <button className="px-4 py-2 mt-4 bg-black text-white rounded-md" onClick={AppleLogin}>Continue with Apple</button>
       <button className="px-4 py-2 mt-4 bg-orange-600 rounded-md" onClick={GuestLogin}>Continue as Guest</button>
     </div>
