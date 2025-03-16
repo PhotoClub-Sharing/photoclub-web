9365d7f7b8052d94aba9b59d2af8692bdbf387c7
Amina Shellman <aminashellman@gmail.com>
2025-03-16 13:51:36 -0700

    comment out download code

--- photoclub-web/pages/fetchAlbums.tsx
+++ photoclub-web/pages/fetchAlbums.tsx
@@ -8,13 +8,12 @@ export default async function FetchAlbums() {
   const albumList = Array.from(querySnapshot.docs.map((doc) => doc.data()));
   return albumList;
 }
-export async function DownloadAlbums(albumid: string) {
-  const Storage = FirebaseInit().Storage();
-  const picRef = Storage.ref("images");
-  const albumList = await Promise.all(
-    albums.map(async (album) => {
-      const id = albumid;
-      const picUrl = await picRef.child(`${id}/*.jpeg`).getDownloadURL()
-      return picUrl;
-    }))
-}
+//export async function DownloadAlbums(albumid: string) {  const Storage = FirebaseInit().Storage();
+//  const picRef = Storage.ref("images");
+  //const albumList = await Promise.all(
+    //albums.map(async (album) => {
+      //const id = albumid;
+    //  const picUrl = await picRef.child(`${id}/*.jpeg`).getDownloadURL()
+     // return picUrl;
+   // }))
+//}
