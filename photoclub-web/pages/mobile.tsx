57a825afab2903af4ab8ac344ac56d41ef673bf7
Amina Shellman <aminashellman@gmail.com>
2025-03-16 14:27:21 -0700

    Fix link

--- photoclub-web/pages/mobile.tsx
+++ photoclub-web/pages/mobile.tsx
@@ -2,7 +2,7 @@ export default function Mobile() {
   return (
     <div className="flex flex-col items-center justify-ce67nter w-full max-w-md p-8 mx-auto bg-white rounded-lg shadow-lg">
       <h1>Mobile App Demo</h1>
-      <a href="https://hc-cdn.hel1.your-objectstorage.com/s/v3/b187ced9ef1181274815237557e9d38eb3359b94_simulator_screen_recording_-_iphone_16_-_2025-03-16_at_14.17.09.mp4">Video of mobile app</a>
+      <a href="https://hc-cdn.hel1.your-objectstorage.com/s/v3/bd4ceb9fb1f2c4b188483744283f8715929af723_simulator_screen_recording_-_iphone_16_-_2025-03-16_at_14.17.09-encoded.mp4">Video of mobile app</a>
     </div>
   );
 }
