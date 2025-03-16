28dbfbffac9f868f7adace9bbd241d210953c3d1
Amina Shellman <aminashellman@gmail.com>
2025-03-16 13:57:16 -0700

    comment ut fetechAlbums

--- photoclub-web/pages/fetchAlbums.tsx
+++ photoclub-web/pages/fetchAlbums.tsx
@@ -1,13 +1,13 @@
 import FirebaseInit from "./initfirebasee"
 import {query, getDocs, collection } from "firebase/firestore";
 import { getStorage, ref, getDownloadURL } from "firebase/storage";
-export default async function FetchAlbums() {
-  const db = FirebaseInit();
-  const q = query(collection(db, "albums"));
-  const querySnapshot = await getDocs(q);
-  const albumList = Array.from(querySnapshot.docs.map((doc) => doc.data()));
-  return albumList;
-}
+//export default async function FetchAlbums() {
+ // const db = FirebaseInit();
+//  const q = query(collection(db, "albums"));
+  //const querySnapshot = await getDocs(q);
+ // const albumList = Array.from(querySnapshot.docs.map((doc) => doc.data()));
+ // return albumList;
+//}
 //export async function DownloadAlbums(albumid: string) {  const Storage = FirebaseInit().Storage();
 //  const picRef = Storage.ref("images");
   //const albumList = await Promise.all(
