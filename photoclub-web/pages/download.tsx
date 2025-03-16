0868ef771a4d302887b52ed6c32623403d6ae9e9
Amina Shellman <aminashellman@gmail.com>
2025-03-16 13:49:24 -0700

    remove old stuff to make it build

---
+++ photoclub-web/pages/download.tsx
@@ -0,0 +1,11 @@
+import Layout from './layout';
+
+export default function Download() {
+  return (
+  <Layout>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 align-content-center gap-16 sm:p-20 font-[family-name:var(--font-lilita-one)] bg-[#FFBC51]">
+    <p className="text-xl">Download coming soon!</p>
+    </div>
+  </Layout>
+  )
+}
