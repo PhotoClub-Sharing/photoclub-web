137bf20d83f42a49977ae7e2f7e9c81cddcf106c
Amina Shellman <aminashellman@gmail.com>
2025-03-16 14:25:08 -0700

    Add mobile demo link

--- photoclub-web/pages/mobile.tsx
+++ photoclub-web/pages/mobile.tsx
@@ -2,6 +2,7 @@ export default function Mobile() {
   return (
     <div className="flex flex-col items-center justify-ce67nter w-full max-w-md p-8 mx-auto bg-white rounded-lg shadow-lg">
       <h1>Mobile App Demo</h1>
+      <a href="https://hc-cdn.hel1.your-objectstorage.com/s/v3/b187ced9ef1181274815237557e9d38eb3359b94_simulator_screen_recording_-_iphone_16_-_2025-03-16_at_14.17.09.mp4">Video of mobile app</a>
     </div>
   );
 }
